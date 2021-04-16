08bc7eff5f135742b6ebebb0e83bd726232a1eea
Dru-Daniels <dancergirl_23@hotmail.com>
2021-04-16 12:05:38 -0400

    Added fetch request GetJobs() to Search Results

---
+++ client/src/components/pages/JobShow.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+import './jobShow.scss'
+
+const JobShow = ({ job }) => {
+  return(
+    <h1>Welcome to the Job Show Page!</h1>
+  )
+}
+
+export default JobShow
--- client/src/components/searchResults/SearchResults.js
+++ client/src/components/searchResults/SearchResults.js
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React, { useEffect, useState } from 'react' 
 
 import ResultFilter from './resultFilter/ResultFilter'
 import ResultTable from './resultTable/ResultTable'
@@ -6,13 +6,36 @@ import ResultTable from './resultTable/ResultTable'
 import './searchResults.scss'
 
 const SearchResults = (props) => {
+  
+  const [jobs, setJobs] = useState([])
+  
+  const getJobs = async () => {
+    try {
+      const response = await fetch(`/api/v1/jobs`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        throw new Error(errorMessage)
+      }
+      const body = await response.json()
+      setJobs(body.jobs)
+    } catch (error) {
+      console.error(error)
+      console.error(`Error in fetch ${error.message}`)
+    }
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    getJobs()
+  }, [])
+
   return(
     <>
       <h3 className='results-title'>Results</h3>
       <div>
         <ResultFilter />
       </div>
-      <ResultTable />
+      <ResultTable jobs={jobs} />
     </>
   )
 }
