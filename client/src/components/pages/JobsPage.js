dc501c8c621f41d38534b5f5e14070b16ff1878c
Tom Low <thomaslsternberg@gmail.com>
2021-04-16 16:18:19 -0400

    moved jobs state to jobspage

--- client/src/components/pages/JobsPage.js
+++ client/src/components/pages/JobsPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Search from '../search/Search'
 import SearchResults from '../searchResults/SearchResults'
@@ -6,6 +6,22 @@ import SearchResults from '../searchResults/SearchResults'
 import './jobsPage.scss'
 
 const JobsPage = (props) => {
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch("/api/v1/jobs")
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`)
+      }
+      const responseBody = await JSON.parse(response)
+      const jobs = responseBody.jobs
+      setJobs(jobs)
+    } catch (error) {
+      console.error(`Error in fetch: ${error}`)
+    }
+  }
+
   return (
     <div className='job-page-container'>
       <div className='job-search-wrapper'>
@@ -14,10 +30,10 @@ const JobsPage = (props) => {
         <h3 className='job-search-subtitle'>
           Your dream job is waiting
         </h3>
-        <Search/>
+        <Search searchJobs={searchJobs} />
       </div>
       <div className='search-results-wrapper'>
-        <SearchResults />
+        <SearchResults jobs={jobs} />
       </div>
       <img src='https://image.shutterstock.com/shutterstock/photos/1620022015/display_1500/stock-vector-vector-illustration-aimed-at-the-goal-increase-motivation-way-to-achieve-the-goal-teamwork-help-1620022015.jpg' alt='Helping Hand' />
     </div>
--- client/src/components/searchResults/SearchResults.js
+++ client/src/components/searchResults/SearchResults.js
@@ -1,35 +1,32 @@
-import React, { useEffect, useState } from 'react' 
+import React, { useEffect } from 'react'
 
 import ResultFilter from './resultFilter/ResultFilter'
 import ResultTable from './resultTable/ResultTable'
 
 import './searchResults.scss'
 
-const SearchResults = (props) => {
-  
-  const [jobs, setJobs] = useState([])
-  
-  const getJobs = async () => {
-    try {
-      const response = await fetch(`/api/v1/jobs`)
-      if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`
-        throw new Error(errorMessage)
-      }
-      const body = await response.json()
-      setJobs(body.jobs)
-    } catch (error) {
-      console.error(error)
-      console.error(`Error in fetch ${error.message}`)
-    }
-  }
+const SearchResults = ({ jobs }) => {
+
+  // const getJobs = async () => {
+  //   try {
+  //     const response = await fetch(`/api/v1/jobs`)
+  //     if (!response.ok) {
+  //       const errorMessage = `${response.status} (${response.statusText})`
+  //       throw new Error(errorMessage)
+  //     }
+  //     const body = await response.json()
+  //     setJobs(body.jobs)
+  //   } catch (error) {
+  //     console.error(error)
+  //     console.error(`Error in fetch ${error.message}`)
+  //   }
+  // }
 
   useEffect(() => {
     window.scrollTo(0, 0)
-    getJobs()
   }, [])
 
-  return(
+  return (
     <>
       <h3 className='results-title'>Results</h3>
       <div>
