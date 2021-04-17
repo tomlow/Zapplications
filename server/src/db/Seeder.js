085b40f3749b788944baa74cb5fb775785f50e87
Tom Low <thomaslsternberg@gmail.com>
2021-04-17 18:03:29 -0400

    job seeders and user seeders

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,8 +1,12 @@
 import JobSeeder from "./seeders/JobSeeder.js"
+import UserSeeder from "./seeders/UserSeeder.js"
 import { connection } from "../boot.js"
 
 class Seeder {
   static async seed() {
+    console.log("Seeding users...")
+    await UserSeeder.seed()
+
     console.log("Seeding jobs...")
     await JobSeeder.seed()
 
---
+++ server/src/db/seeders/UserSeeder.js
@@ -0,0 +1,21 @@
+import User from "../../models/User.js"
+
+const company1 = { name: "Google", email: "theOGmail@gmail.com", password: "encryptedPass", location: "Mountain View, CA", isEmployer: true, picture: "https://openthread.google.cn/images/ot-contrib-google.png", description: "Google is what google does. You know us. We're google. Don't know who we are? Google it." }
+
+const company2 = {
+  name: "NFTerrible", email: "dont@me.com", password: "narcissus", location: "San Francisco, CA", isEmployer: true, picture: "https://openthread.google.cn/images/ot-contrib-google.png", description: "We will bend your stardust body to the needs of our stockholders."
+}
+
+const ordinaryGuy = {
+  name: "Doug Itup", email: "getitup@gmail.com", password: "stilllearning", location: "Boston, MA", github: "github.com/ditup", linkedin: "linkedin.com/in/doug", phone: "9784718747", isEmployer: false, picture: "https://openthread.google.cn/images/ot-contrib-google.png"
+}
+
+class UserSeeder {
+  static async seed() {
+    await User.query().insert(company1)
+    await User.query().insert(company2)
+    await User.query().insert(ordinaryGuy)
+  }
+}
+
+export default UserSeeder
